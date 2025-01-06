a2b5c500e5eceab274dbbf8c23328b34866b6e17
Avishay Spitzer <avishaysp@gmail.com>
2025-01-06 20:21:14 +0200

    initialize client socket, status, and map handling

--- Excercize 3 files/cman_client.py
+++ Excercize 3 files/cman_client.py
@@ -14,6 +14,10 @@ class Client:
 
     def __init__(self, server_address):
         self.server_address = server_address
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.status = Client.Status.WAITING
+        self.map_matrix = MapReader(MAP_PATH).read_map()
+        self.map_converter = MapConverter()
 
     def run(self):
         while self.status != Client.Status.GAME_OVER:
@@ -21,7 +25,29 @@ class Client:
             self._refresh_display()
 
     def _refresh_display(self):
+        data = self.__recv_data()
+        if data is None:
+            return
+        self.__update_map(data)
+        clear_print(self.map_converter.convert_to_string(self.map_matrix))
+
+    def __recv_data(self):
+        last_data = None
+        while True:
+            # we want to return the last packet, discard all packets before
+            ready, _, _ = select.select([self.socket], [], [], 0)
+            if not ready:
+                break
+            
+            raw, addr = self.socket.recvfrom(BUFFER_SIZE)
+            if addr == self.server_address:
+                last_data = raw.decode(CODING)
+        
+        return last_data
+    
+    def __update_map(self, data):
         pass
+    
 
     def _process_user_input(self):
         pass
