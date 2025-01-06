d68a198b13d9c17e0447ae1ab3263b7882dfe31c
Avishay Spitzer <avishaysp@gmail.com>
2025-01-06 20:20:55 +0200

    add status to client

--- Excercize 3 files/cman_client.py
+++ Excercize 3 files/cman_client.py
@@ -1,13 +1,22 @@
+from enum import Enum
+import socket
+import select
 from time import sleep
 from cman_utils import get_pressed_keys, clear_print
-from consts import PLAYER_MOVEMENT, QUIT
+from consts import BUFFER_SIZE, CODING, PLAYER_MOVEMENT, QUIT, MAP_PATH
+from client_map import MapReader, MapConverter
 
 class Client:
+    class Status(Enum):
+        WAITING = 0
+        PLAYING = 1
+        GAME_OVER = 2
+
     def __init__(self, server_address):
         self.server_address = server_address
 
     def run(self):
-        while True:
+        while self.status != Client.Status.GAME_OVER:
             self._process_user_input()
             self._refresh_display()
 
