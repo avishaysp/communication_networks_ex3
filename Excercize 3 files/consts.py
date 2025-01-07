5e412cc2e1fb81dc29273f508d0e47691e4eb3ae
Dory Grossman (GitHub2) <dorygrossman@mail.tai.ac.il>
2025-01-07 15:36:24 +0200

    created server

--- Excercize 3 files/consts.py
+++ Excercize 3 files/consts.py
@@ -1,4 +1,4 @@
-SERVER_ADDR = '127.0.0.1'
+SERVER_ADDR = '0.0.0.0'
 DEFAULT_PORT = 1337
 BUFFER_SIZE = 1024
 
@@ -11,3 +11,16 @@ GAME_END = 0x8F
 ERROR = 0xFF
 
 MAP_PATH = './map.txt'
+
+ERROR_DICT = [
+    'bad format error, message prefix is not correct',
+    'Unknown client',
+    'bad format error, join request role is not correct',
+    'Client is already in the game',
+    'cman position is already taken',
+    'ghost position is already taken',
+    "Game not started yet, can't move",
+    'bad format error, move is not correct',
+    'Non players are not allowed to send move commands',
+    'bad format error, quit request is not correct'
+]
