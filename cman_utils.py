9cf2bf87c8b6f037eee936b1ccc76049edbb9e4a
Avishay Spitzer <avishaysp@gmail.com>
2025-01-08 20:56:22 +0200

    add timeout to client user input

--- cman_utils.py
+++ cman_utils.py
@@ -1,6 +1,7 @@
 import sys
 import tty
 import termios
+import select
 
 def _flush_input():
     try:
@@ -11,18 +12,22 @@ def _flush_input():
         import sys, termios
         termios.tcflush(sys.stdin, termios.TCIOFLUSH)
 
-def get_single_char():
-    """Reads a single character from standard input."""
+def get_single_char(timeout=0.1):
+    """Reads a single character from standard input with a timeout."""
     fd = sys.stdin.fileno()
     old_settings = termios.tcgetattr(fd)
     try:
         tty.setraw(fd)
-        char = sys.stdin.read(1)
+        rlist, _, _ = select.select([sys.stdin], [], [], timeout)
+        if rlist:
+            char = sys.stdin.read(1)
+        else:
+            char = None
     finally:
         termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
     return char
 
-def get_pressed_keys(keys_filter = None):
+def get_pressed_keys(keys_filter=None):
     """
     
     Returns a list of all pressed keys at the time of the call.
@@ -36,12 +41,10 @@ def get_pressed_keys(keys_filter = None):
     list[str]: A list of currently pressed keys.
 
     """
-    print("Press any key to continue...")
     ch = get_single_char()
     if keys_filter:
         return [ch] if ch in keys_filter else []
-    print("Pressed key:", ch)
-    return [ch]
+    return [ch] if ch else []
 
 def clear_print(*args, **kwargs):
     """
